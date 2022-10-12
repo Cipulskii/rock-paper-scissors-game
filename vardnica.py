2137ed7cc32490810f2732bde35236dca80de1a4
Cipulskii <114050628+Cipulskii@users.noreply.github.com>
2022-10-12 09:29:28 +0300

    vardnica, bet gara

---
+++ vardnica.py
@@ -0,0 +1,10 @@
+contact_anna = {'name': 'Anna', 'age': 17}
+contact_oskars = {'name': 'Oskars', 'age': 22}
+contact_jenifer = {'name': 'Jenifer', 'age': 18}
+contact_humpis = {'name': 'Humpis', 'age': 70}
+
+contacts = [contact_anna, contact_oskars, contact_jenifer, contact_humpis]
+
+for contact in contacts:
+    print(contact['name'])
+    print(contact['age'])
