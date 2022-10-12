49627136e877f55a880bd35d627405f21301a280
Cipulskii <114050628+Cipulskii@users.noreply.github.com>
2022-10-12 09:30:06 +0300

    vardnica bet iisa

---
+++ vardniica_2.py
@@ -0,0 +1,5 @@
+contacts = [{'name': 'Anna', 'age': 17}, {'name': 'Oskars', 'age': 22}, {'name': 'Jenifer', 'age': 18}, {'name': 'Humpis', 'age': 70}]
+
+for contact in contacts:
+    print(contact['name'])
+    print(contact['age'])
